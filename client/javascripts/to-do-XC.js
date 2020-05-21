738a544b7be4be131e0a43c04eb10d12def178a3
River Lane Weston <river.weston123@gmail.com>
2020-05-21 11:05:57 -0700

    Extra Credit Complete

---
+++ client/javascripts/to-do-XC.js
@@ -0,0 +1,52 @@
+//jshint esversion: 6
+
+let controller = function() {
+  console.log(localStorage.getItem("toDoList"));
+
+  if (localStorage.getItem("toDoList")) {
+    $(".comments").html(localStorage.getItem("toDoList"));
+  }
+
+  let addCommentFromInputBox = function() {
+    //Semmy uses "$" to name variables that will contain jQuery objects
+    let $new_comment;
+
+    if ($(".comment-input input").val() !== "") {
+      $new_comment = $("<p>").text($(".comment-input input").val());
+      //$new_comment.hide();
+      $(".comments").append($new_comment);
+      //$new_comment.fadeIn();
+      $(".comment-input input").val("");
+
+      localStorage.setItem("toDoList", $(".comments").html());
+      //log the list of paragraph elements
+      console.log($(".comments").html());
+    }
+  };
+
+  //
+
+  $(".comment-input button").on("click", function(event) {
+    addCommentFromInputBox();
+  });
+
+  $(".comment-input input").on("keypress", function(event) {
+    if (event.keyCode === 13) {
+      addCommentFromInputBox();
+    }
+  });
+};
+
+let clearButton = () => {
+  localStorage.removeItem("toDoList")
+  window.location.reload();
+}
+
+$(document).ready(() => {
+  let buttonElem = document.querySelectorAll("button")[1];
+  buttonElem.addEventListener("click", clearButton);
+  controller();
+});
+
+
+$(document).ready(controller);
